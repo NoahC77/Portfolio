2a0b2d1b2901695a47d2d017e44b91ad2eebb629
NoahC77 <noahcastetter@gmail.com>
2022-01-08 10:55:25 -0600

    Add Github link button and email button to top of the page. Add navbar starter.

---
+++ portfolio/src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+	import Button from './Button.svelte'
+	import Navbar from './Navbar.svelte'
+	import Sidebar from './Sidebar.svelte'
+
+	let open = false;
+	export let name;
+
+</script>
+
+
+
+<main>
+	<h1>Noah Castetter</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Button><a href="https://github.com/NoahC77">Github</a></Button>
+	<Button>noahcastetter@gmail.com</Button>
+
+
+	<Navbar bind:sidebar={open}/>
+	<Sidebar bind:open/>
+</main>
+
+<svelte:head>
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+</svelte:head>
+
+<style>
+	@import 'https://unpkg.com/@responsive-ui/button@latest/lib/index.css';
+
+	:global(body){
+		padding: 0;
+	}
+	
+	main {
+		background-color: #93c64e;
+		text-align: left;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ffaccc;
+		text-transform: uppercase;
+		font-family: Courier New;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	a {
+		color: white;	
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
